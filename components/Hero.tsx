01e730fd5ec95dc8a789d33ef685e2b693c4c082
Bambo Bello <user@Bambo.local>
2023-11-11 11:44:49 +0100

    first commit👽

---
+++ components/Hero.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <section className='border-2 border-red-500  max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:ppy-20  xl:flex-row'>
+      <div className='hero-map' />
+
+    </section>
+  )
+}

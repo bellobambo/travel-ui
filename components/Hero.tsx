63e64b8c70670ee44b434536eedd662c7b5b0f14
Bambo Bello <user@Bambo.local>
2023-11-11 11:49:39 +0100

    hero👽

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,13 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function Hero() {
   return (
     <section className='border-2 border-red-500  max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:ppy-20  xl:flex-row'>
       <div className='hero-map' />
+      <div className='relative z-30 flex flex-1 flex-col xl:w-1/2'>
+        <Image src='/camp.svg' alt='camp' width={50} height={50} />
+      </div>
 
     </section>
   )

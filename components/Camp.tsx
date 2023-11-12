f9dc62b4a52044ba4d45b439278cfd97ef2073b7
Bambo Bello <user@Bambo.local>
2023-11-12 10:54:40 +0100

    commit👽

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -49,8 +49,8 @@ const CampSite = ({ backgroundImage, title, subtitle, peopleJoined }: CampProps)
 
 const Camp = () => {
   return (
-    <section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
-      <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
+    <section className=" padding-container 2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20 p-6">
+      <div className="  hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
         <CampSite
           backgroundImage="bg-bg-img-1"
           title="Putuk Truno Camp"
@@ -85,5 +85,6 @@ const Camp = () => {
     </section>
   )
 }
+
 
 export default Camp
